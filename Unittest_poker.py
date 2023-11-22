18ea3365d6203f08a6766a32c7a8aa3eba298389
Dominik Schatz <doschatz@tsn.at>
2023-11-22 18:26:27 +0100

    update

--- Unittest_poker.py
+++ Unittest_poker.py
@@ -21,19 +21,19 @@ class TestCardFunctions(unittest.TestCase):
         cards_per_suit = 13
         self.assertFalse(paar(selected_cards, cards_per_suit))
 
-    # def test_flush(self):
-    #     # Test for the flush function
-    #     # You can create specific test cases to cover different scenarios
-    #
-    #     # Test case 1: Test when there is a flush in the selected cards
-    #     selected_cards = [1, 2, 3, 4, 5]
-    #     cards_per_suit = 13
-    #     self.assertTrue(flush(selected_cards, cards_per_suit))
-    #
-    #     # Test case 2: Test when there is no flush in the selected cards
-    #     selected_cards = [1, 2, 3, 4, 13]
-    #     cards_per_suit = 13
-    #     self.assertFalse(flush(selected_cards, cards_per_suit))
+    def test_flush(self):
+        # Test for the flush function
+        # You can create specific test cases to cover different scenarios
+
+        # Test case 1: Test when there is a flush in the selected cards
+        selected_cards = [1, 3, 7, 9, 5]
+        cards_per_suit = 13
+        self.assertTrue(flush(selected_cards, cards_per_suit))
+
+        # Test case 2: Test when there is no flush in the selected cards
+        selected_cards = [1, 2, 3, 4, 13]
+        cards_per_suit = 13
+        self.assertFalse(flush(selected_cards, cards_per_suit))
 
 if __name__ == '__main__':
     unittest.main()
