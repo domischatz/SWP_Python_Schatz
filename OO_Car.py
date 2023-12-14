62a44901918b81eb35f31314961cf79a3632893b
Dominik Schatz <doschatz@tsn.at>
2023-12-14 08:49:47 +0100

    update

---
+++ OO_Car.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"Color: {self.color}, Mileage: {self.mileage}"
+
+car1 = Car(color="blue", mileage=20_000)
+car2 = Car(color="red", mileage=30_000)
+
+print(car1)
+print(car2)
